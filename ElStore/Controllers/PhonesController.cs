f92c801eadc3021eb8247727e77c918ad8210936
maksimbilyk <pdo090318@gmail.com>
2024-04-21 23:06:49 +0300

    Test in Upsert

--- ElStore/Controllers/PhonesController.cs
+++ ElStore/Controllers/PhonesController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using ElStore.Data;
 using ElStore.Models;
 using ElStore.Models.ViewModel;
+using Microsoft.EntityFrameworkCore;
 
 namespace ElStore.Controllers;
 
@@ -34,46 +35,107 @@ public class PhonesController : Controller
     }
 
     
+    //Upsert - get
     public IActionResult Upsert(int? id)
     {
+        
+        var video = _db.Images.Where(u => u.Id == id).Select(i => i.Video).FirstOrDefault();
+        var images = _db.Images.Where(i => i.Id == id).Select(i => i.Image).ToList();
+        Product? product = _db.Product.Find(id);
+        DetailsVM phone = new DetailsVM
+        {
+            Product = product,
+            DescriptionPc = _db.DescriptionPC.FirstOrDefault(d => d != null && d.Id == id),
+            HearphoneDescriptions = null,
+            Video = video,
+            Image = images
+        };
+        
         if (id == null)
         {
-            DetailsVM phone = new DetailsVM();
             return View(phone);
         }
         else
         {
-            Product? product = _db.Product.Find(id);
-            if (product == null)
+            phone.Product = _db.Product.Find(id);
+            if (phone.Product == null)
             {
                 return NotFound();
             }
-            
-            var video = _db.Images.Where(u=> u.Id == id).Select(i => i.Video).FirstOrDefault();
-            var images = _db.Images
-                .Where(i => i.Id == id)
-                .Select(i => i.Image)
-                .ToList();
-            DetailsVM phone = new DetailsVM
-            {
-                Product = product,
-                DescriptionPc = _db.DescriptionPC.FirstOrDefault(d => d != null && d.Id == id),
-                HearphoneDescriptions = null,
-                Video = video,
-                Image = images
-                
-            };
             return View(phone);
         }
     }
-    
+
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public IActionResult Upsert(DetailsVM phone)
+    public IActionResult Upsert(DetailsVM phone, List<IFormFile> imageFiles, IFormFile videoFile, int? id)
     {
-        return View(phone);
+        var video = _db.Images.Where(u => u.Id == id).Select(i => i.Video).FirstOrDefault();
+        var images = _db.Images.Where(i => i.Id == id).Select(i => i.Image).ToList();
+        Product? product = _db.Product.Find(id);
+        DetailsVM phones = new DetailsVM
+        {
+            Product = product,
+            DescriptionPc = _db.DescriptionPC.FirstOrDefault(d => d != null && d.Id == id),
+            HearphoneDescriptions = null,
+            Video = video,
+            Image = images
+        };
+        
+        if (!ModelState.IsValid)
+        {
+            if (phone.Product != null && phone.Product.Id == 0)
+            {
+                //create a product
+                Console.WriteLine("Create");
+                var existingCategory = _db.Category.Find(phone.Product.CategoryId);
+                if (existingCategory != null)
+                {
+                    _db.Product.Add(phone.Product);
+                    _db.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            else
+            {
+                //update
+                Console.WriteLine("Update");
+                if (phones.Product != null && phones.DescriptionPc != null)
+                {
+                    phones.Product.Brand = phone.Product.Brand;
+                    phones.Product.Model = phone.Product.Model;
+                    phones.Product.ShortDescription = phone.Product.ShortDescription;
+                    phones.Product.Battery = phone.Product.Battery;
+                    phones.Product.Price = phone.Product.Price;
+                    phones.DescriptionPc.RAM = phone.Product.DescriptionPC.RAM;
+                    phones.DescriptionPc.ROM = phone.Product.DescriptionPC.ROM;
+                    phones.DescriptionPc.Display = phone.Product.DescriptionPC.Display;
+                    phones.DescriptionPc.FrontCamera = phone.Product.DescriptionPC.FrontCamera;
+                    phones.DescriptionPc.BackCamera = phone.Product.DescriptionPC.BackCamera;
+                    phones.DescriptionPc.Text = phone.Product.DescriptionPC.Text;
+                    phones.Video = "n0b4y9snzCQ";
+                    _db.SaveChanges();
+                }
+                
+
+                _db.SaveChanges();
+            }
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            return NotFound();
+        }
     }
+
+
     
+    
+    //Details - get
     public IActionResult Details(int? id)
     {
         if (id == null)
@@ -102,12 +164,11 @@ public class PhonesController : Controller
         return View(detailsVm);
     }
     
+    //Details - post
     [HttpPost, ActionName("Details")]
     public IActionResult DetailsPost(int id, DetailsVM detailsVm)
     {
         return View(detailsVm);
     }
     
-    
-
 }
---
+++ ElStore/Migrations/20240420105857_ChangeInDescriptionPC.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ElStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeInDescriptionPC : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "DescriptionPC");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "DescriptionPC",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
